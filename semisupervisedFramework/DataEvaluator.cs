a5317ffc8e4d4173091c591f0f1e108fc692f94f
toddhaugen <todd@haugen.name>
2019-06-28 13:51:07 -0700

    forst working blob move

---
+++ semisupervisedFramework/DataEvaluator.cs
@@ -0,0 +1,387 @@
+using System;
+using System.Configuration;
+using System.IO;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using Microsoft.Azure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.WindowsAzure.Storage.DataMovement;
+
+namespace semisupervisedFramework
+{
+    public static class DataEvaluator
+    {
+        [FunctionName("EvaluateData")]
+
+        public static void Run([BlobTrigger("pendingevaluation/{blobName}", Connection = "AzureWebJobsStorage")]Stream myBlob, string blobName, ILogger log)
+        {
+            string pendingEvaluationStorageContainerName = "pendingevaluation";
+            string evaluatedDataStorageContainerName = "evaluateddata";
+            //use the following lines if you want to have the user enter the account name and account key from the command line.
+            //Console.WriteLine("Enter Storage account name:");
+            //string accountName = Console.ReadLine();
+
+            //Console.WriteLine("\nEnter Storage account key:");
+            //string accountKey = Console.ReadLine();
+
+            //string storageConnectionString = "DefaultEndpointsProtocol=https;AccountName=" + accountName + ";AccountKey=" + accountKey;
+            //CloudStorageAccount account = CloudStorageAccount.Parse(storageConnectionString);
+
+            // Create Reference to Azure Storage Account
+            String storageConnection = "DefaultEndpointsProtocol=https;AccountName=semisupervisedstorage;AccountKey=XBHB5fxDqFAZQLRzcN/C/QLiR+55obGzE7hDdRjSsD07mcNwmpwFnH2MZWayPajGSiXRl4wO3rUFbKiXnSpPaw==;EndpointSuffix=core.windows.net";
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(storageConnection);
+
+            //Get a reference to a container, if the container does not exist create one then get the reference to the blob you want to evaluate."
+            CloudBlockBlob dataEvaluating = GetBlob(storageAccount, pendingEvaluationStorageContainerName, blobName);
+            CloudBlockBlob evaluatedData = GetBlob(storageAccount, evaluatedDataStorageContainerName, blobName);
+
+            TransferAzureBlobToAzureBlob(storageAccount, dataEvaluating, evaluatedData).Wait();
+
+            log.LogInformation($"C# Blob trigger function Processed blob\n Name:{blobName} \n Size: {myBlob.Length} Bytes");
+        }
+        public static async Task TransferAzureBlobToAzureBlob(CloudStorageAccount account, CloudBlockBlob sourceBlob, CloudBlockBlob destinationBlob)
+        {
+            TransferCheckpoint checkpoint = null;
+            SingleTransferContext context = GetSingleTransferContext(checkpoint);
+            CancellationTokenSource cancellationSource = new CancellationTokenSource();
+            //Console.WriteLine("\nTransfer started...\nPress 'c' to temporarily cancel your transfer...\n");
+
+            Stopwatch stopWatch = Stopwatch.StartNew();
+            Task task;
+            ConsoleKeyInfo keyinfo;
+            try
+            {
+                task = TransferManager.CopyAsync(sourceBlob, destinationBlob, true, null, context, cancellationSource.Token);
+                while (!task.IsCompleted)
+                {
+                    if (Console.KeyAvailable)
+                    {
+                        keyinfo = Console.ReadKey(true);
+                        if (keyinfo.Key == ConsoleKey.C)
+                        {
+                            cancellationSource.Cancel();
+                        }
+                    }
+                }
+                await task;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("\nThe transfer is canceled: {0}", e.Message);
+            }
+
+            if (cancellationSource.IsCancellationRequested)
+            {
+                Console.WriteLine("\nTransfer will resume in 3 seconds...");
+                Thread.Sleep(3000);
+                checkpoint = context.LastCheckpoint;
+                context = GetSingleTransferContext(checkpoint);
+                Console.WriteLine("\nResuming transfer...\n");
+                await TransferManager.CopyAsync(sourceBlob, destinationBlob, false, null, context, cancellationSource.Token);
+            }
+
+            stopWatch.Stop();
+            Console.WriteLine("\nTransfer operation completed in " + stopWatch.Elapsed.TotalSeconds + " seconds.");
+            //ExecuteChoice(account);
+        }
+        //Gets a reference to a specific blob
+        public static CloudBlockBlob GetBlob(CloudStorageAccount account, string containerName, string blobName)
+        {
+            CloudBlobClient blobClient = account.CreateCloudBlobClient();
+            CloudBlobContainer container = blobClient.GetContainerReference(containerName);
+            container.CreateIfNotExistsAsync().Wait();
+
+            CloudBlockBlob blob = container.GetBlockBlobReference(blobName);
+
+            return blob;
+        }
+        public static SingleTransferContext GetSingleTransferContext(TransferCheckpoint checkpoint)
+        {
+            SingleTransferContext context = new SingleTransferContext(checkpoint);
+
+            context.ProgressHandler = new Progress<TransferStatus>((progress) =>
+            {
+                Console.Write("\rBytes transferred: {0}", progress.BytesTransferred);
+            });
+
+            return context;
+        }
+    }
+
+    public class Program
+    {
+        public static SingleTransferContext GetSingleTransferContext(TransferCheckpoint checkpoint)
+        {
+            SingleTransferContext context = new SingleTransferContext(checkpoint);
+
+            context.ProgressHandler = new Progress<TransferStatus>((progress) =>
+            {
+                Console.Write("\rBytes transferred: {0}", progress.BytesTransferred);
+            });
+
+            return context;
+        }
+
+        public static DirectoryTransferContext GetDirectoryTransferContext(TransferCheckpoint checkpoint)
+        {
+            DirectoryTransferContext context = new DirectoryTransferContext(checkpoint);
+
+            context.ProgressHandler = new Progress<TransferStatus>((progress) =>
+            {
+                Console.Write("\rBytes transferred: {0}", progress.BytesTransferred);
+            });
+
+            return context;
+        }
+
+        public static void SetNumberOfParallelOperations()
+        {
+            Console.WriteLine("\nHow many parallel operations would you like to use?");
+            string parallelOperations = Console.ReadLine();
+            TransferManager.Configurations.ParallelOperations = int.Parse(parallelOperations);
+        }
+
+        public static string GetSourcePath()
+        {
+            Console.WriteLine("\nProvide path for source:");
+            string sourcePath = Console.ReadLine();
+
+            return sourcePath;
+        }
+
+        public static CloudBlockBlob GetBlob(CloudStorageAccount account)
+        {
+            CloudBlobClient blobClient = account.CreateCloudBlobClient();
+
+            Console.WriteLine("\nProvide name of Blob container:");
+            string containerName = Console.ReadLine();
+            CloudBlobContainer container = blobClient.GetContainerReference(containerName);
+            container.CreateIfNotExistsAsync().Wait();
+
+            Console.WriteLine("\nProvide name of Blob:");
+            string blobName = Console.ReadLine();
+            CloudBlockBlob blob = container.GetBlockBlobReference(blobName);
+
+            return blob;
+        }
+
+        public static CloudBlobDirectory GetBlobDirectory(CloudStorageAccount account)
+        {
+            CloudBlobClient blobClient = account.CreateCloudBlobClient();
+
+            Console.WriteLine("\nProvide name of Blob container:");
+            string containerName = Console.ReadLine();
+            CloudBlobContainer container = blobClient.GetContainerReference(containerName);
+            container.CreateIfNotExistsAsync().Wait();
+
+            CloudBlobDirectory blobDirectory = container.GetDirectoryReference("");
+
+            return blobDirectory;
+        }
+
+        public static async Task TransferLocalFileToAzureBlob(CloudStorageAccount account)
+        {
+            string localFilePath = GetSourcePath();
+            CloudBlockBlob blob = GetBlob(account);
+            TransferCheckpoint checkpoint = null;
+            SingleTransferContext context = GetSingleTransferContext(checkpoint);
+            CancellationTokenSource cancellationSource = new CancellationTokenSource();
+            Console.WriteLine("\nTransfer started...\nPress 'c' to temporarily cancel your transfer...\n");
+
+            Stopwatch stopWatch = Stopwatch.StartNew();
+            Task task;
+            ConsoleKeyInfo keyinfo;
+            try
+            {
+                task = TransferManager.UploadAsync(localFilePath, blob, null, context, cancellationSource.Token);
+                while (!task.IsCompleted)
+                {
+                    if (Console.KeyAvailable)
+                    {
+                        keyinfo = Console.ReadKey(true);
+                        if (keyinfo.Key == ConsoleKey.C)
+                        {
+                            cancellationSource.Cancel();
+                        }
+                    }
+                }
+                await task;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("\nThe transfer is canceled: {0}", e.Message);
+            }
+
+            if (cancellationSource.IsCancellationRequested)
+            {
+                Console.WriteLine("\nTransfer will resume in 3 seconds...");
+                Thread.Sleep(3000);
+                checkpoint = context.LastCheckpoint;
+                context = GetSingleTransferContext(checkpoint);
+                Console.WriteLine("\nResuming transfer...\n");
+                await TransferManager.UploadAsync(localFilePath, blob, null, context);
+            }
+
+            stopWatch.Stop();
+            Console.WriteLine("\nTransfer operation completed in " + stopWatch.Elapsed.TotalSeconds + " seconds.");
+            //ExecuteChoice(account);
+        }
+
+        public static async Task TransferLocalDirectoryToAzureBlobDirectory(CloudStorageAccount account)
+        {
+            string localDirectoryPath = GetSourcePath();
+            CloudBlobDirectory blobDirectory = GetBlobDirectory(account);
+            TransferCheckpoint checkpoint = null;
+            DirectoryTransferContext context = GetDirectoryTransferContext(checkpoint);
+            CancellationTokenSource cancellationSource = new CancellationTokenSource();
+            Console.WriteLine("\nTransfer started...\nPress 'c' to temporarily cancel your transfer...\n");
+
+            Stopwatch stopWatch = Stopwatch.StartNew();
+            Task task;
+            ConsoleKeyInfo keyinfo;
+            UploadDirectoryOptions options = new UploadDirectoryOptions()
+            {
+                Recursive = true
+            };
+
+            try
+            {
+                task = TransferManager.UploadDirectoryAsync(localDirectoryPath, blobDirectory, options, context, cancellationSource.Token);
+                while (!task.IsCompleted)
+                {
+                    if (Console.KeyAvailable)
+                    {
+                        keyinfo = Console.ReadKey(true);
+                        if (keyinfo.Key == ConsoleKey.C)
+                        {
+                            cancellationSource.Cancel();
+                        }
+                    }
+                }
+                await task;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("\nThe transfer is canceled: {0}", e.Message);
+            }
+
+            if (cancellationSource.IsCancellationRequested)
+            {
+                Console.WriteLine("\nTransfer will resume in 3 seconds...");
+                Thread.Sleep(3000);
+                checkpoint = context.LastCheckpoint;
+                context = GetDirectoryTransferContext(checkpoint);
+                Console.WriteLine("\nResuming transfer...\n");
+                await TransferManager.UploadDirectoryAsync(localDirectoryPath, blobDirectory, options, context);
+            }
+
+            stopWatch.Stop();
+            Console.WriteLine("\nTransfer operation completed in " + stopWatch.Elapsed.TotalSeconds + " seconds.");
+            //ExecuteChoice(account);
+        }
+
+        public static async Task TransferUrlToAzureBlob(CloudStorageAccount account)
+        {
+            Uri uri = new Uri(GetSourcePath());
+            CloudBlockBlob blob = GetBlob(account);
+            TransferCheckpoint checkpoint = null;
+            SingleTransferContext context = GetSingleTransferContext(checkpoint);
+            CancellationTokenSource cancellationSource = new CancellationTokenSource();
+            Console.WriteLine("\nTransfer started...\nPress 'c' to temporarily cancel your transfer...\n");
+
+            Stopwatch stopWatch = Stopwatch.StartNew();
+            Task task;
+            ConsoleKeyInfo keyinfo;
+            try
+            {
+                task = TransferManager.CopyAsync(uri, blob, true, null, context, cancellationSource.Token);
+                while (!task.IsCompleted)
+                {
+                    if (Console.KeyAvailable)
+                    {
+                        keyinfo = Console.ReadKey(true);
+                        if (keyinfo.Key == ConsoleKey.C)
+                        {
+                            cancellationSource.Cancel();
+                        }
+                    }
+                }
+                await task;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("\nThe transfer is canceled: {0}", e.Message);
+            }
+
+            if (cancellationSource.IsCancellationRequested)
+            {
+                Console.WriteLine("\nTransfer will resume in 3 seconds...");
+                Thread.Sleep(3000);
+                checkpoint = context.LastCheckpoint;
+                context = GetSingleTransferContext(checkpoint);
+                Console.WriteLine("\nResuming transfer...\n");
+                await TransferManager.CopyAsync(uri, blob, true, null, context, cancellationSource.Token);
+            }
+
+            stopWatch.Stop();
+            Console.WriteLine("\nTransfer operation completed in " + stopWatch.Elapsed.TotalSeconds + " seconds.");
+            //ExecuteChoice(account);
+        }
+
+        public static async Task TransferAzureBlobToAzureBlob(CloudStorageAccount account)
+        {
+            CloudBlockBlob sourceBlob = GetBlob(account);
+            CloudBlockBlob destinationBlob = GetBlob(account);
+            TransferCheckpoint checkpoint = null;
+            SingleTransferContext context = GetSingleTransferContext(checkpoint);
+            CancellationTokenSource cancellationSource = new CancellationTokenSource();
+            //Console.WriteLine("\nTransfer started...\nPress 'c' to temporarily cancel your transfer...\n");
+
+            Stopwatch stopWatch = Stopwatch.StartNew();
+            Task task;
+            ConsoleKeyInfo keyinfo;
+            try
+            {
+                task = TransferManager.CopyAsync(sourceBlob, destinationBlob, true, null, context, cancellationSource.Token);
+                while (!task.IsCompleted)
+                {
+                    if (Console.KeyAvailable)
+                    {
+                        keyinfo = Console.ReadKey(true);
+                        if (keyinfo.Key == ConsoleKey.C)
+                        {
+                            cancellationSource.Cancel();
+                        }
+                    }
+                }
+                await task;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("\nThe transfer is canceled: {0}", e.Message);
+            }
+
+            if (cancellationSource.IsCancellationRequested)
+            {
+                Console.WriteLine("\nTransfer will resume in 3 seconds...");
+                Thread.Sleep(3000);
+                checkpoint = context.LastCheckpoint;
+                context = GetSingleTransferContext(checkpoint);
+                Console.WriteLine("\nResuming transfer...\n");
+                await TransferManager.CopyAsync(sourceBlob, destinationBlob, false, null, context, cancellationSource.Token);
+            }
+
+            stopWatch.Stop();
+            Console.WriteLine("\nTransfer operation completed in " + stopWatch.Elapsed.TotalSeconds + " seconds.");
+            //ExecuteChoice(account);
+        }
+    }
+}
