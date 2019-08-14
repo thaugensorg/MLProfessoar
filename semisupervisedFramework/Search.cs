bd47d1dfb11b36ebbd8a2b79714aed872619e2b5
toddhaugen <todd@haugen.name>
2019-08-13 23:07:49 -0700

    Adding Azure Search Indexing

--- semisupervisedFramework/Search.cs
+++ semisupervisedFramework/Search.cs
@@ -7,6 +7,8 @@ using Microsoft.Azure.Search.Models;
 using Microsoft.Spatial;
 using Newtonsoft.Json;
 using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
 
 using Microsoft.Azure.Storage;
 using Microsoft.Azure.Storage.Blob;
@@ -14,6 +16,7 @@ using Microsoft.Azure.Storage.DataMovement;
 
 // http://danielcoding.net/working-with-azure-search-in-c-net/
 // https://docs.microsoft.com/en-us/azure/search/search-howto-dotnet-sdk
+// https://cmatskas.com/indexing-and-searching-sql-server-data-with-azure-search/
 
 namespace semisupervisedFramework
 {
@@ -25,27 +28,14 @@ namespace semisupervisedFramework
             string SearchApiKey = Environment.GetEnvironmentVariable("blobSearchKey", log);
             string indexName = Environment.GetEnvironmentVariable("bindinghash", log);
 
-            SearchServiceClient serviceClient = new SearchServiceClient("semisupervisedBlobSearch", new SearchCredentials(SearchApiKey));
+            //instanciates a serch client and creates the index.
+            SearchServiceClient serviceClient = Helper.Initialize("semisupervisedblobsearch", "blobindex", SearchApiKey);
+            DataSource JsonBlob = Helper.CreateBlobSearchDataSource(log);
+            serviceClient.DataSources.CreateOrUpdateAsync(JsonBlob).Wait();
+            Index BlobIndex = Helper.CreateIndex(serviceClient, "blobindex");
+            Task<Indexer> BlobIndexer = Helper.CreateBlobIndexer(serviceClient, BlobIndex, "json-blob");
+            serviceClient.Indexers.RunAsync(BlobIndexer)
 
-            Console.WriteLine("{0}", "Deleting index...\n");
-            indexClient = serviceClient.Indexes(indexName);
-                
-                DeleteIndexIfExists(indexName, serviceClient);
-
-            Console.WriteLine("{0}", "Creating index...\n");
-            serviceClient.CreateIndex(indexName, serviceClient);
-
-            ISearchIndexClient indexClient = serviceClient.Indexes.GetClient(indexName);
-
-            Console.WriteLine("{0}", "Uploading documents...\n");
-            UploadDocuments(indexClient);
-
-            ISearchIndexClient indexClientForQueries = CreateSearchIndexClient(configuration);
-
-            RunQueries(indexClientForQueries);
-
-            Console.WriteLine("{0}", "Complete.  Press any key to end application...\n");
-            Console.ReadKey();
         }
     }
     public class DataBlob
@@ -63,16 +53,15 @@ namespace semisupervisedFramework
     }
     public static class Helper
     {
+        // https://cmatskas.com/indexing-and-searching-sql-server-data-with-azure-search/
         public static SearchServiceClient Initialize(string serviceName, string indexName, string apiKey)
         {
             SearchServiceClient serviceClient = new SearchServiceClient(serviceName, new SearchCredentials(apiKey));
-            DeleteIfIndexExist(serviceClient, indexName);
-            CreateIndex(serviceClient, indexName);
 
             return serviceClient;
         }
 
-        private static void CreateIndex(SearchServiceClient client, string indexName)
+        public static Index CreateIndex(SearchServiceClient client, string indexName)
         {
             // https://docs.microsoft.com/en-us/rest/api/searchservice/create-index
             var indexDefinition = new Index()
@@ -88,7 +77,11 @@ namespace semisupervisedFramework
                 }
             };
 
+            DeleteIfIndexExist(client, indexName);
+
             client.Indexes.Create(indexDefinition);
+
+            return client.Indexes.Get(indexName);
         }
 
         private static void DeleteIfIndexExist(SearchServiceClient client, string indexName)
@@ -98,91 +91,35 @@ namespace semisupervisedFramework
                 client.Indexes.Delete(indexName);
             }
         }
-    }
-    public class Uploader
-    {
-        private List<DataBlob> PrepareDocuments()
+
+        public static DataSource CreateBlobSearchDataSource(ILogger log)
         {
-            List<DataBlob> carDocuments = new List<DataBlob>();
+            string StorageConnection = Environment.GetEnvironmentVariable("AzureWebJobsStorage", log);
 
-            // Create Reference to Azure Storage Account
-            CloudStorageAccount StorageAccount = Environment.GetStorageAccount(log);
-            CloudBlobClient BlobClient = StorageAccount.CreateCloudBlobClient();
-            CloudBlobContainer PendingEvaluationContainer = BlobClient.GetContainerReference("pendingevaluation");
-            string TargetBlobUrl;
-            foreach (IListBlobItem item in PendingEvaluationContainer.ListBlobs(null, false))
-            {
-                if (item.GetType() == typeof(CloudBlockBlob))
-                {
-                    CloudBlockBlob blob = (CloudBlockBlob)item;
-                    TargetBlobUrl = blob.Uri.ToString();
-                }
-            }
-
-            DataBlob c1 = new DataBlob()
-            {
-                Id = "1",
-                Name = "Proton Iriz",
-                Category = "Hatchback",
-                LaunchDate = new DateTimeOffset(2015, 1, 20, 0, 0, 0, TimeSpan.Zero),
-                Price = 55000,
-                SafetyRating = 5
-            };
-
-            DataBlob c2 = new DataBlob()
-            {
-                Id = "2",
-                Name = "Perodua Myvi",
-                Category = "Hatchback",
-                LaunchDate = new DateTimeOffset(2004, 6, 15, 0, 0, 0, TimeSpan.Zero),
-                Price = 40000,
-                SafetyRating = 3
-            };
-
-            DataBlob c3 = new DataBlob()
-            {
-                Id = "3",
-                Name = "Perodua Axia",
-                Category = "Hatchback",
-                LaunchDate = new DateTimeOffset(2014, 12, 25, 0, 0, 0, TimeSpan.Zero),
-                Price = 30000,
-                SafetyRating = 2
-            };
-
-            DataBlob c4 = new DataBlob()
-            {
-                Id = "4",
-                Name = "BMW 320i Sport",
-                Category = "Sedan",
-                LaunchDate = new DateTimeOffset(2000, 8, 31, 0, 0, 0, TimeSpan.Zero),
-                Price = 300000,
-                SafetyRating = 4
-            };
-
-            carDocuments.Add(c1);
-            carDocuments.Add(c2);
-            carDocuments.Add(c3);
-            carDocuments.Add(c4);
-
-            return carDocuments;
+            DataSource dataSource = DataSource.AzureBlobStorage(
+                name: "json-blob",
+                storageConnectionString: StorageConnection,
+                containerName: "json");
+            return dataSource;
         }
 
-        public void Upload(ISearchIndexClient indexClient)
+        public static async Task<Indexer> CreateBlobIndexer(SearchServiceClient searchService, Index index, string dataSourceName)
         {
-            try
-            {
-                var documents = PrepareDocuments();
-                var batch = IndexBatch.Upload(documents);
-                indexClient.Documents.Index(batch);
+            var indexer = new Indexer(
+                name: "blob-indexer",
+                dataSourceName: dataSourceName,
+                targetIndexName: index.Name,
+                schedule: new IndexingSchedule(TimeSpan.FromMinutes(1)));
 
-                Thread.Sleep(2000);
-            }
-            catch (IndexBatchException e)
+            var exists = await searchService.Indexers.ExistsAsync(indexer.Name);
+            if (exists)
             {
-                Console.WriteLine(
-                    $"Oops! The following index failed...\n { e.IndexingResults.Where(r => !r.Succeeded).Select(r => r.Key) }");
+                await searchService.Indexers.ResetAsync(indexer.Name);
             }
+
+            await searchService.Indexers.CreateOrUpdateAsync(indexer);
+
+            return indexer;
         }
-
     }
 }
