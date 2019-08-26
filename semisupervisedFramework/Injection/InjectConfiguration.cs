b5bb2c670c8571c11ba0dc3911fc8cd333806d32
Jerry Nixon <jerry@nixoncorp.com>
2019-08-26 13:15:27 -0600

    Introducing Dependency Injection

---
+++ semisupervisedFramework/Injection/InjectConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Azure.WebJobs.Host.Config;
+using Microsoft.Extensions.DependencyInjection;
+using semisupervisedFramework.Settings;
+using System;
+
+namespace semisupervisedFramework.Injection
+{
+    public class InjectConfiguration : IExtensionConfigProvider
+    {
+        private IServiceProvider _serviceProvider;
+
+        public void Initialize(ExtensionConfigContext context)
+        {
+            var services = new ServiceCollection();
+            RegisterServices(services);
+            _serviceProvider = services.BuildServiceProvider(true);
+
+            context
+                .AddBindingRule<InjectAttribute>()
+                .BindToInput<dynamic>(i => _serviceProvider.GetRequiredService(i.Type));
+        }
+
+        private void RegisterServices(IServiceCollection services)
+        {
+            services.AddSingleton<Search, Search>();
+            services.AddSingleton<Engine, Engine>();
+        }
+    }
+}
