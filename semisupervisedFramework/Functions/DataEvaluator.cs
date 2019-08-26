cd9d483b90db1a8051a256f4173539c067b99b87
Jerry Nixon <jerry@nixoncorp.com>
2019-08-26 14:19:37 -0600

    Moving the ZeroLength class to Exception folder, I missed it earlier.

--- semisupervisedFramework/Functions/DataEvaluator.cs
+++ semisupervisedFramework/Functions/DataEvaluator.cs
@@ -30,6 +30,7 @@ using Microsoft.AspNetCore.Http;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using semisupervisedFramework.Exceptions;
+using semisupervisedFramework.Blob;
 
 namespace semisupervisedFramework.Functions
 {
